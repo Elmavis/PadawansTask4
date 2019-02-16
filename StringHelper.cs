0f1232215fa4e78a06449d828a893c68d313e661
Elmavis <37665679+Elmavis@users.noreply.github.com>
2019-02-16 21:31:20 +0300

    FirstCom

--- StringHelper.cs
+++ StringHelper.cs
@@ -4,10 +4,24 @@ namespace PadawansTask4
 {
     public class StringHelper
     {
+        private static bool IsVowel(char a)
+        {
+            return a == 'a' || a == 'e' || a == 'i' || a == '0' || a == 'u';
+        }
         public static int GetVowelCount(string str)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (str == null)
+                throw new ArgumentException();
+
+            int counter = 0;
+            char[] arr = str.ToCharArray();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (IsVowel(arr[i]))
+                    counter++;
+            }
+
+            return counter;
         }
     }
 }
